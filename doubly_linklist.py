7d5aed436399363499e0e32223ffc0dda0f0ab1d
nomi181472 <nomansoomro51@gmail.com>
2019-12-22 02:17:17 +0500

    doubly

---
+++ doubly_linklist.py
@@ -0,0 +1,74 @@
+
+
+class my_node:
+    def __init__(self, data):
+        self.next = None
+        self.prev = None
+        self.data = data
+
+
+class doubly:
+    def __init__(self):
+        self.head = None
+        self.counter = -1
+
+    def append(self, data):
+        if self.head is None:
+            my_new_node = my_node(data)
+            self.head = my_new_node
+            self.counter = self.counter+1
+
+        else:
+            my_new_node = my_node(data)
+            curr = self.head
+            while(curr.next):
+                curr = curr.next
+            curr.next = my_new_node
+            my_new_node.prev = curr
+            my_new_node.next = None
+            self.counter = self.counter+1
+
+    def at_direct_index(self, index, data):
+        if(self.counter <= index):
+            self.append(data)
+            return
+        my_new_node = my_node(data)
+        if(index == 0):
+            temp = self.head
+            my_new_node.next = self.head
+            self.head = my_new_node
+            return
+
+        curr = self.head
+        i = 0
+        while(i <= index-1):
+            i = i+1
+            curr = curr.next
+        temp = curr.next
+        curr.next = my_new_node
+        my_new_node.prev = curr
+        my_new_node.next = temp
+
+    def Queue(self, data):
+        if self.head is None:
+            self.append(data)
+            return
+        my_new_node = my_node(data)
+        temp = self.head
+        my_new_node.next = self.head
+        self.head = my_new_node
+
+    def display(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+
+
+if __name__ == "__main__":
+    obj = doubly()
+    obj.Queue(3)
+    obj.Queue(4)
+    obj.Queue(6)
+    obj.Queue(7)
+    obj.display()
